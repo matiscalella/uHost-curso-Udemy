8e59ebda97a5597cd0a62fccfcacc8dc05320cb6
Matias Scalella <matias.scalella@live.com>
2018-07-02 16:18:23 -0300

    agrego animaciones y efectos

--- shared.js
+++ shared.js
@@ -4,11 +4,15 @@ var selectPlanButtons = document.querySelectorAll('.plan button');
 var noButton = document.querySelector('.modal__action--negative');
 var toggleButton = document.querySelector('.toggle-button');
 var mobileNav = document.querySelector('.mobile-nav');
+var ctaButton = document.querySelector('.main-nav__item--cta');
 
 for (var i = 0; i < selectPlanButtons.length; i++ ) {
     selectPlanButtons[i].addEventListener('click', function() {
         modal.classList.add('open');
-        backdrop.classList.add('open');
+        backdrop.style.display = 'block';
+        setTimeout(function() {
+            backdrop.classList.add('open');
+        }, 10)
     });
 }
 
@@ -26,10 +30,27 @@ function closeModal() {
         modal.classList.remove('open');
     }
     backdrop.classList.remove('open');
+    setTimeout(function() {
+        backdrop.style.display = 'none';
+    }, 200)
 }
 
 toggleButton.addEventListener('click', function() {
     mobileNav.classList.add('open');
-    backdrop.classList.add('open');
+    backdrop.style.display = 'block';
+    setTimeout(function() {
+        backdrop.classList.add('open');
+    }, 10)
 });
 
+ctaButton.addEventListener('animationstart', function(event) {
+    console.log('Animation started', event);
+})
+
+ctaButton.addEventListener('animationend', function(event) {
+    console.log('Animation ended', event);
+})
+
+ctaButton.addEventListener('animationiteration', function(event) {
+    console.log('Animation iteration', event);
+})
